5513ffe296859c8bf121170daf4185a1f1e3e9be
Sylverstone <sylvio0801@gmail.com>
2024-01-05 17:13:03 -0400

    correction

--- docs/Js/footer.js
+++ docs/Js/footer.js
@@ -4,12 +4,14 @@ document.write(`
         <div>
             <h3>Mon établissement</h3>
             <p>Lycée Faustin Fléret</p>
-            <p>Guadeloupe, Morne-à-l'Eau</p>
             <p>Général et Technologique</p>
+            <p>Morne-a-l'eau, Guadeloupe</p>
+            
         </div>
         <div>
             <h3>Contactez-moi</h3>
-            <a class="lien_email" href="mailto:sylvio0801@gmail.com" target="_blank" rel="noopener">sylvio0801@gmail.com</a>  
+            <a class="lien_email" href="mailto:sylvio0801@gmail.com" target="_blank" rel="noopener">@ : sylvio0801@gmail.com</a>
+            <p>TEL : +590690417031</p>  
         </div>
 </footer>
 <button class="retour" onclick="retour_en_haut()">⬆️</button>
