78925d8af7ac66b2ba8a7182b84a86ccfe4587d5
Sylverstone <sylvio0801@gmail.com>
2024-08-03 14:43:09 -0400

    possibilité de voir en plus les images de projets ajouté.

--- Js/afficheImageProjet.js
+++ Js/afficheImageProjet.js
@@ -1,3 +1,4 @@
+
 function afficheImage(basePath, baseImage, nbImage,element){
     let paths = [];
     for(let i = 0; i < nbImage; i++){
@@ -9,12 +10,15 @@ function afficheImage(basePath, baseImage, nbImage,element){
             console.log(path);
             const imgElement = document.createElement("img");
             imgElement.src = path;
+            imgElement.style.cursor
+            imgElement.onclick = () => afficheImageGrand(imgElement,path);
             imgElement.alt = "image du projet";
             element.appendChild(imgElement);
         });
     }
 
 }
+
 
 let AllContainerImageProjet = document.querySelectorAll(".imageProjet");
 
@@ -29,3 +33,26 @@ const liste = [
 AllContainerImageProjet.forEach( (element,i) => {
     afficheImage(liste[i][0],liste[i][1],liste[i][2],element);
 })
+
+function closeAfficheImage(){
+    let afficheurImage = document.querySelector(".afficheurImageVisible");
+    let image = afficheurImage.querySelector("img");
+    if(image && afficheurImage){
+        afficheurImage.removeChild(image);
+        afficheurImage.className = "afficheurImageCache";
+    }
+    
+}
+
+function afficheImageGrand(imgElement,path){
+    let afficheurImage = document.querySelector(".afficheurImageCache");
+    if (afficheurImage) {
+        afficheurImage.className = "afficheurImageVisible";
+        let imageCopy = imgElement.cloneNode(true);
+        imageCopy.onclick = () => {
+            window.open(path,"_blank");
+        }
+        afficheurImage.appendChild(imageCopy);
+    }
+    
+}
