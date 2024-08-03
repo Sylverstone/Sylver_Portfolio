8c68be64c8c6621c22dd8ac766330710b5eaa3a0
Sylverstone <sylvio0801@gmail.com>
2024-08-03 11:17:58 -0400

    changement design du nav en position fixed

--- Js/navScript.js
+++ Js/navScript.js
@@ -1,14 +1,15 @@
 let nav = document.querySelector("nav");
 let header = document.querySelector("header");
 origine_nav_top = header.offsetHeight
+
 window.onscroll = () => {
     let top_nav = nav.offsetTop
-    if(top_nav < window.scrollY && top_nav != 0){
-        nav.style.top = "0";
-        nav.style.position = "fixed";
-        nav.style.width = "100%";
+    if(top_nav < window.scrollY && top_nav != 0){        
+        nav.className = "naviFlottant"
+
+
     } else if (origine_nav_top > window.scrollY){
-        nav.style.position = "relative";
-        nav.style.top = "none";
+        nav.className = "navi"
+
     }
 };
