26c8d2af51760655ffcd9903bc8dc2a10c945e0b
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2025-02-10 21:42:58 +0100

    debug image qui fonctionne pas

--- src/app/components/ImageProfil.tsx
+++ src/app/components/ImageProfil.tsx
@@ -7,25 +7,30 @@ import styles from "@/style/styles.module.scss"
 const ImageProfil = () => {
 
     const nbImage = 3;
-    const [ImgPos,setImgPos] = useState(0);
-    
+    const [ImgPos,setImgPos] = useState(1);
+    const [src, setsrc] = useState("/photoProfil/photo_profil1.jpg");
+    const [isOnAnim, setisOnAnim] = useState(false);
     const handleClick = (e : React.MouseEvent<HTMLImageElement, MouseEvent>) =>
     {
-        const target = e.currentTarget;
-
-        target.classList.add(styles.blur);target.classList.add(styles.hidden)
-        setTimeout(() => {
-            target.classList.remove(styles.hidden);
-            setImgPos((ImgPos + 1) % nbImage);
-            target.setAttribute("src",`/photoProfil/photo_profil${ImgPos + 1}.jpg`);
-        }, 1000);
-        
+        if(!isOnAnim)
+        {
+            const target = e.currentTarget;
+            setisOnAnim(true);
+            target.classList.add(styles.blur);target.classList.add(styles.hidden)
+            setTimeout(() => {
+                target.classList.remove(styles.hidden);
+                setisOnAnim(false);
+                setImgPos((ImgPos + 1) % nbImage);
+                setsrc(`/photoProfil/photo_profil${ImgPos + 1}.jpg`);
+            }, 1000);
+        }
+    
     }
 
     return (
         <>
             <Image 
-                src={`/photoProfil/photo_profil${ImgPos + 1}.jpg`}
+                src={src}
                 onClick={(e) => handleClick(e)}
                 alt="portrait Sylvio Pelage Maxime"
                 width={1000}
