72fc493e7e9c0c409bcfd1fd95b4fc518b457cc0
Sylvio PELAGE--MAXIME <sylvio0801@gmail.com>
2026-02-25 15:22:55 +0100

    Changement du style, du projet phare

--- views/Template/project.php
+++ views/Template/project.php
@@ -17,35 +17,42 @@
             <a href="Projets/<?=$keys[$i]?>" alt="Lien vers le projet : <?=$keys[$i]?> ">
                 <img class="Illustration" loading="lazy" src="Images/Projet/<?=$keys[$i]?>.webp" alt="$<?=$keys[$i]?> - Image" project="<?=$keys[$i]?>" />
             </a>
-            <p><?=$projet['Title']?></p>
+            <?php if($projet["categorie"] !== 2) : ?>
+                <p><?=$projet['Title']?></p>
+            <?php endif ?>
         </aside>
-        <div class="CompContainer">
-            <?php foreach($projet["Comp"] as $key => $comp) : ?>
-                <?php if($projet["Helps"][$key] != "") : ?>
-                    <span class="Comp-<?=$comp?>" onclick="help(this,10)">
-                            <?=$comp?>
-                            &#9432;
-                            <div class="tips">
-                                <?=$projet['Helps'][$key]?>
-                            </div>
+        <section class="MainProjectContainer">
+            <?php if($projet["categorie"] === 2) : ?>
+                <h2 class="text-centered"><?=$projet['Title']?></h2>
+            <?php endif ?>
+            <div class="CompContainer">
+                <?php foreach($projet["Comp"] as $key => $comp) : ?>
+                    <?php if($projet["Helps"][$key] != "") : ?>
+                        <span class="Comp-<?=$comp?> cliquable" onclick="help(this,10)">
+                                <?=$comp?>
+                                &#9432;
+                                <div class="tips">
+                                    <?=$projet['Helps'][$key]?>
+                                </div>
                         </span>
-                <?php else : ?>
-                    <span class="Comp-<?=$comp?>">
-                            <?=$comp?>
-                        </span>
-                <?php endif; ?>
-            <?php endforeach?>
-        </div>
-        <span class="DescriptionProjet"><?=$projet["Desc"]?></span>
-        <article class="contribution">
-            <p>Ma contribution :</p>
-            <ul>
-                <?php foreach($projet["Contributions"] as $contribution) : ?>
-                    <li><?= $contribution ?></li>
+                    <?php else : ?>
+                        <span class="Comp-<?=$comp?>">
+                                <?=$comp?>
+                            </span>
+                    <?php endif; ?>
                 <?php endforeach?>
-            </ul>
-            <span class="Date"><?= $projet["Date"]?></span>
-        </article>
+            </div>
+            <span class="DescriptionProjet"><?=$projet["Desc"]?></span>
+            <article class="contribution">
+                <p>Ma contribution :</p>
+                <ul>
+                    <?php foreach($projet["Contributions"] as $contribution) : ?>
+                        <li><?= $contribution ?></li>
+                    <?php endforeach?>
+                </ul>
+                <span class="Date"><?= $projet["Date"]?></span>
+            </article>
+        </section>
     </div>
 
     <?php if($i + 1 < count($keys) && $projets[$keys[$i + 1]]["categorie"] != $lastCat) : ?>
